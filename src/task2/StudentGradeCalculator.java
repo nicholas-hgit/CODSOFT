aa754e9498cb3868ac4902ccc819d77654824564
nicholas-hgit <nicholas.hlungwane@gmail.com>
2024-03-04 14:19:06 +0200

    replaced methods with print statements

--- src/task2/StudentGradeCalculator.java
+++ src/task2/StudentGradeCalculator.java
@@ -1,5 +1,5 @@
 package task2;
-import java.util.InputMismatchException;
+
 import java.util.Scanner;
 
 public class StudentGradeCalculator {
@@ -8,71 +8,49 @@ public class StudentGradeCalculator {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter total number of subjects");
-
-        int numberOfSubjects = getNumberOfSubjects();
+        int numberOfSubjects = input.nextInt();
 
         double totalMarks = 0;
         double mark;
-        for(int subjectNumber = 1; subjectNumber <= numberOfSubjects;) {
+
+        for (int subjectNumber = 1; subjectNumber <= numberOfSubjects; ) {
             System.out.printf("Enter mark for subject %d out of 100 %n", subjectNumber);
 
-            try {
                 mark = input.nextDouble();
-                if(isValidMark(mark)){
+
+                if (isValidMark(mark)) {
+
                     totalMarks += mark;
                     ++subjectNumber;
-                }
-                else {
+
+                } else {
                     System.out.println("Invalid mark");
                 }
 
-            }catch (InputMismatchException e) {
-                input.nextLine();
-                System.out.println("Invalid mark");
-            }
         }
 
-        double studentAverage = calculateStudentAverage(totalMarks,numberOfSubjects);
+        double studentAverage = calculateStudentAverage(totalMarks, numberOfSubjects);
         char studentGrade = getStudentGradeFromAverage(studentAverage);
 
-        printStudentTotalMarks(totalMarks);
-        printStudentAverage(studentAverage);
-        printStudentGrade(studentGrade);
+        System.out.printf("Total marks: %s%n", totalMarks);
+        System.out.printf("Average percentage: %s%n", studentAverage);
+        System.out.printf("Grade: %s%n", studentGrade);
 
     }
 
-    public static int getNumberOfSubjects () {
-        Scanner input = new Scanner(System.in);
-
-        int numberOfSubjects = 0;
-        boolean isValidInput = false;
-
-        do {
-            try {
-                numberOfSubjects = input.nextInt();
-                isValidInput = true;
-            }catch (InputMismatchException e) {
-                input.nextLine();
-                System.out.println("number of subjects must be an integer");
-            }
-
-        }while (!isValidInput);
-
-        return  numberOfSubjects;
-    }
 
     public static boolean isValidMark(double mark) {
         return mark >= 0 && mark <= 100;
     }
 
-    public static  double calculateStudentAverage(double totalMarks, int numberOfSubjects) {
+    public static double calculateStudentAverage(double totalMarks, int numberOfSubjects) {
         return totalMarks / numberOfSubjects;
     }
 
     public static char getStudentGradeFromAverage(double average) {
         char grade;
 
-        if(average > 75) {
+        if (average > 75) {
             grade = 'A';
         } else if (average > 70) {
             grade = 'B';
@@ -82,25 +60,10 @@ public class StudentGradeCalculator {
             grade = 'D';
         } else if (average >= 50) {
             grade = 'E';
-        }
-        else {
+        } else {
             grade = 'F';
         }
 
         return grade;
     }
-
-    public static void printStudentTotalMarks(double marks){
-        System.out.printf("Total marks: %s%n",marks);
-    }
-
-    public static void printStudentAverage(double average) {
-        System.out.printf("Average percentage: %s%n", average);
-    }
-
-    public static void printStudentGrade(char grade) {
-        System.out.printf("Grade: %s%n", grade);
-    }
 }
-
-
